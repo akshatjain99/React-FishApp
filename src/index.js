9e3a9227c3b2c14cbfd94c889ec81963454977f3
Akshat Jain <jain.akshat546@gmail.com>
2018-12-13 13:28:06 +0530

    Created layout and basic routing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {render} from 'react-dom';
 import StorePicker from './components/StorePicker';
 import './css/style.css';
+import App from './components/App';
+import Router from './components/Router';
 
 
-render(<StorePicker />, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
