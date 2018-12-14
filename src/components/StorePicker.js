1a8642034a3989f2d9f97185df11b014a48e850d
Akshat Jain <jain.akshat546@gmail.com>
2018-12-14 23:45:33 +0530

    Created fishes state and methods to update it

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { getFunName } from '../helpers.js';
 
 class StorePicker extends React.Component{
+	myInput = React.createRef();
+
+	goToStore = (event) => {
+		//Stop the form from submitting and reloading
+		event.preventDefault();
+		const storeName = this.myInput.value.value;
+		this.props.history.push(`/store/${storeName}`);
+	}
+
 	render(){
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore}>
 				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder="Store Name" />
+				<input 
+					type="text"
+					ref={this.myInput} 
+					required placeholder="Store Name" 
+					defaultValue={getFunName()}/>
 				<button type="submit">Visit Store</button>
 			</form>
 		);
